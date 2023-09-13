055f4b3dffd6d03b4f4c71f196f8b141b9eee871
ChaeheunKim <123622993+ChaeheunKim@users.noreply.github.com>
2023-09-13 16:09:20 +0900

    Update arithmeticShort2.cpp

--- practice/week2/arithmeticShort2.cpp
+++ practice/week2/arithmeticShort2.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-	int two = 2;
-	int eight = 8;
-	int sum1 = 2;
-	int sum2 = 2;
+	int two = 2;   //two 변수에 정수 2 값 할당
+	int eight = 8;  //eight 변수에 정수 8 값 할당
+	int sum1 = 2;   //sum1 변수에 정수 2 값 할당
+	int sum2 = 2;    //sum2 변수에 정수 2 값 할당
 
-	sum1 *= two;      //sum1=sum1*two
-	sum2 /= eight;    //sum2=sum2/eight
+	sum1 *= two;      //sum1 변수에 sum1 에 2 곱한 값 할당
+	sum2 /= eight;    //sum2 변수에 sum2 에 8 나눈 값 할당
 
 	cout << "sum1 = " << sum1 << endl;
 	cout << "sum2 = " << sum2 << endl;
