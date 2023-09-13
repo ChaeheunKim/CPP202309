b10dc995693e7d2c7c1a12c9813e77fc1c099e2f
ChaeheunKim <123622993+ChaeheunKim@users.noreply.github.com>
2023-09-13 16:01:38 +0900

    Update IncrementDecrement.cpp

--- practice/week2/IncrementDecrement.cpp
+++ practice/week2/IncrementDecrement.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-	int x = 1; //x를 1로 초기화
+	int x = 1; //변수 x에 정수 1 할당
 	cout << "x = " << x++ << endl; //x 출력 후 x에 1 더하기
 	cout << "x = " << x++ << endl; //x 출력 후 x에 1 더하기
 	cout << "x = " << ++x << endl; //x에 1 더한 후 출력하기
