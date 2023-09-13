5bfee2321f6e9bf104323beba8bae3ebcab9a76f
ChaeheunKim <123622993+ChaeheunKim@users.noreply.github.com>
2023-09-13 16:06:38 +0900

    Update cout2.cpp

--- practice/week2/cout2.cpp
+++ practice/week2/cout2.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main() {
 	char character = 'C';  //문자형 변수에 C 할당
 	int integer = 100;    //정수형 변수에 100 할당
-	double precision = 3.14159;    //precision 는 실수형
-	bool is_true = true;        //is_true는 논리형
-	string word = "Hello World"; //word는 문자열
-
+	double precision = 3.14159;    //실수형 변수에 3.14159 할당
+	bool is_true = true;        //논리형 변수에 true 할당
+	string word = "Hello World"; //문자열 변수에 Hello World 할당
+ 
 	cout << "char : " << character << endl;
 	cout << "int : " << integer << endl;
 	cout << "double : " << precision << endl;
 	cout << "bool : " << is_true << endl;
-	cout << "string : " << word << endl;
+	cout << "string : " << word << endl; //출력
 
 	return 0;
 }
