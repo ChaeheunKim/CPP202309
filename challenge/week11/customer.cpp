a584284531f880f4270d43f54717dae9fba945a0
chaeheun <alzl7410@gmail.com>
2023-11-19 13:23:44 +0900

    week11 customer 개선

---
+++ challenge/week11/customer.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    int maxPeople;
+    cout << "최대 사람 수를 입력하세요: ";
+    cin >> maxPeople;
+
+    vector<string> names(maxPeople); // 문자열 벡터 선언
+    vector<int> ages(maxPeople);     // 정수 벡터 선언
+
+    for (int i = 0; i < maxPeople; ++i) {
+        cout << "사람" << i + 1 << "의 이름: ";
+        cin >> names[i];
+        cout << "사람" << i + 1 << "의 나이: ";
+        cin >> ages[i];
+    }
+
+    int ageThreshold;
+    cout << "특정 나이 이상인 사람을 찾으려면 나이를 입력하세요: ";
+    cin >> ageThreshold;
+
+    cout << ageThreshold << "세 이상인 고객들:\n";
+    int detectedPeople = 0;
+    for (int i = 0; i < maxPeople; i++) {
+        if (ages[i] >= ageThreshold) {
+            cout << names[i] << " (" << ages[i] << "세)\n";
+            detectedPeople++;
+        }
+    }
+    if (detectedPeople == 0) {
+        cout << ageThreshold << "이상의 나이를 가진 고객이 없습니다";
+    }
+
+    return 0;
+}
